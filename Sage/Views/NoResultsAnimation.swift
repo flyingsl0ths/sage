eb4dd7934b481e41e05adcd002c24140522d03d9
flyingsl0ths <randomemail33110@protonmail.com>
2025-04-28 10:37:16 -0700

    Update animation

--- Sage/Views/NoResultsAnimation.swift
+++ Sage/Views/NoResultsAnimation.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct NoResultsAnimation: View {
     @State private var rotation: CGFloat = 0
     @State private var textOpacity: CGFloat = 0
+    @State private var iconScale: CGFloat = 0.5
 
     var body: some View {
         VStack(spacing: 20) {
-            Icon(iconName: "face.smiling", iconSize: 58)
+            Icon(iconName: "books.vertical", iconSize: 58)
                 .rotationEffect(.degrees(rotation))
+                .scaleEffect(iconScale)
                 .onAppear {
                     withAnimation(.bouncy(duration: 0.9, extraBounce: 0.25)) {
-                        rotation += 180
+                        iconScale += 0.5
                     }
 
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.30) {
-                        withAnimation(.easeInOut(duration: 0.95)) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.18) {
+                        withAnimation(.easeIn(duration: 0.95)) {
                             textOpacity = 1.0
                         }
                     }
